fc8dc6d38ef1cb399e5d019b97c325898735f858
codingshroom <zyt3xx@gmail.com>
2025-11-04 10:04:27 +0100

    filled out some blanks in logic.py

--- src/logic.py
+++ src/logic.py
@@ -1,6 +1,7 @@
+
 class Logic:
     def __init__(self, cards: dict, start_bucket: set, learning_bucket: set, three_correct_bucket: set):
-        self.all_cards = [Card(**data) for data in cards.values()]
+        self.all_cards = [Card(**data) for data in cards.values(self)]
         self.start_bucket = start_bucket
         self.learning_bucket = learning_bucket
         self.three_correct_bucket = three_correct_bucket
@@ -9,37 +10,48 @@ class Logic:
         self.current_card_index = 0
         self.current_card = self.cards[current_card_index]
         self.is_answer_revealed = False
-        self.card_content = ""
+        self.card_content = self.current_card.answer if self.is_answer_revealed else self.current_card.question
         self.show_right_button = False
         self.show_wrong_button = False
 
-    def next_card():
-        # increase current_card_index by one, looping around if at last index previously
+    def next_card(self):
+        self.current_card_index += 1
+        self.current_card_index %= len(self.current_bucket)
+        return self.current_card
 
-    def flip_card():
-        # switch bool on is_answer_revealed, change card_content accordingly
+    def flip_card(self):
+        self.is_answer_revealed = not self.is_answer_revealed
 
-    def prev_card():
-        # decrease current_card_index by one, looping around if at index 0 previously
+    def prev_card(self):
+        self.current_card_index -= 1
+        self.current_card_index %= len(self.current_bucket)
+        return self.current_card
 
-    def get_answer_right():
-        # add bool: right to card.streak
-        # self.deactivate_feedback_buttons()
+    def get_answer_right(self):
+        card.streak.pop(0)
+        card.streak.append(True)
+        self.deactivate_feedback_buttons()
 
-    def get_answer_wrong():
-        # add bool: wrong to card.streak
-        # self.deactivate_feedback_buttons()
+    def get_answer_wrong(self):
+        card.streak.pop(0)
+        card.streak.append(False)
+        self.deactivate_feedback_buttons()
 
-    def deactivate_feedback_buttons():
+    def activate_feedback_buttons(self):
+        # if no right or wrong button was clicked for current card and run yet
+        self.show_right_button = True
+        self.show_wrong_button = True
+
+    def deactivate_feedback_buttons(self):
         # if right or wrong button was clicked for current card and run
         # double or multi insertion should be prevented
 
-    def change_to_start_bucket():
-        # self.current_bucket = self.start_bucket
+    def change_to_start_bucket(self):
+        self.current_bucket = self.start_bucket
 
-    def change_to_learning_bucket():
-        # self.current_bucket = self.learning_bucket
+    def change_to_learning_bucket(self):
+        self.current_bucket = self.learning_bucket
 
-    def change_to_three_correct_bucket():
-        # self.current_bucket = self.three_correct_bucket
+    def change_to_three_correct_bucket(self):
+        self.current_bucket = self.three_correct_bucket
 
